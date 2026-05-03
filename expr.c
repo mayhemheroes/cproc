7e9a684afa6ebb2b752631904d052afc4677fc6b
Michael Forney <mforney@mforney.org>
2026-05-02 21:55:55 -0700

    expr: Fix hexval alphebetic range
    This was accidentally introduced in 3ea7d077.

--- expr.c
+++ expr.c
@@ -472,9 +472,9 @@ hexval(int c)
 {
 	if ('0' <= c && c <= '9')
 		return c - '0';
-	if ('a' <= c && c <= 'z')
+	if ('a' <= c && c <= 'f')
 		return c - 'a' + 10;
-	if ('A' <= c && c <= 'Z')
+	if ('A' <= c && c <= 'F')
 		return c - 'A' + 10;
 	return -1;
 }
